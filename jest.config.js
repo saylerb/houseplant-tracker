ecbac674a2b8c75b15bb6e6d19768ba6e4ba5c5d
Brian Sayler <sayler.b@gmail.com>
2020-06-12 09:31:30 -0500

    initial commit :sparkles:

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.svelte$": ["jest-transform-svelte"]
+  },
+  moduleFileExtensions: ["js", "svelte"],
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"]
+};
