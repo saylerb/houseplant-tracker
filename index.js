45d4768f8d3d71053731b2ef83d29b25b602fd20
Brian Sayler <sayler.b@gmail.com>
2020-06-21 17:48:20 -0500

    add prisma client and write some plants to the database

---
+++ index.js
@@ -0,0 +1,47 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const plantNames = [
+  "Bird's Nest Fern",
+  "Calathea",
+  "Vick's Plant",
+  "Spider Plant #1",
+  "Jade",
+  "Small Echeveria",
+  "Spidy 2",
+  "Snake Plant",
+  "Cryptanthus",
+  "Large Echeveria",
+  "Alocasia",
+  "Pothos",
+  "String of Pearls",
+  "Spidy 3",
+  "Howarthia",
+  "Philodendron Moonlight",
+  "Button Fern"
+];
+
+async function main() {
+  const plantPromises = plantNames.map(name => {
+    return prisma.plant.create({
+      data: {
+        name
+      }
+    });
+  });
+
+  await Promise.all(plantPromises);
+
+  const allPlants = await prisma.plant.findMany();
+
+  console.log(allPlants);
+}
+
+main()
+  .catch(e => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.disconnect();
+  });
