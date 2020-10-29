269a9cdb148a3d96537c3ed9570be8dc093a4bf2
Brian Sayler <sayler.b@gmail.com>
2020-10-28 21:15:49 -0600

    don't assume that there's only a single cookie on the request

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import compression from "compression";
 import polka from "polka";
 import bodyParser from "body-parser";
 import * as sapper from "@sapper/server";
-import cookie from "cookie"
+import cookie from "cookie";
 import jwt from "jsonwebtoken";
 
 const { PORT, NODE_ENV, JWT_SECRET } = process.env;
@@ -11,12 +11,13 @@ const dev = NODE_ENV === "development";
 
 polka()
   .use((request, response, next) => {
-    if (typeof request.headers.cookie !== "undefined" || request.headers.cookie === "") {
-      const { token } = cookie.parse(request.headers.cookie);
+    const { token } = cookie.parse(request.headers.cookie);
 
+    if (typeof token !== "undefined" || token === "") {
       const { userId } = jwt.verify(token, JWT_SECRET);
       request.userId = userId;
     }
+
     next();
   })
   .use(bodyParser.json())
