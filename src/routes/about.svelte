ca80bfa95248770277cb6a237c471dda1fb4bc5f
Brian Sayler <sayler.b@gmail.com>
2020-10-29 13:19:26 -0600

    actually describe what the app does

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,10 +1,20 @@
+<style>
+  img {
+    max-width: 100%;
+  }
+</style>
+
 <svelte:head>
   <title>About</title>
 </svelte:head>
-
 <h1>Plant Tracker</h1>
 
 <p>
-  This app is meant to serve two purposes: a playground for experimenting with
-  web technologies, and something that will help me keep my plants alive.
+  This app is meant to help YOU keep your plants alive. You can add plants to a
+  list and mark them as watered. The app will sort plants by time elapsed since
+  the last watering. Create an account now and start tracking your plants!
 </p>
+
+<img
+  alt="screen shot of application showing a list of plants and when they were last watered"
+  src="/app-screenshot.png" />
